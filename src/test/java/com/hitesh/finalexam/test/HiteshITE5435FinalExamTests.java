ce4897b35b5596e5e2adc5e825701215059642f7
hitesh4056 <hitesh@ostaapp.com>
2025-04-12 18:07:31 -0400

    Final

--- src/test/java/com/hitesh/finalexam/test/HiteshITE5435FinalExamTests.java
+++ src/test/java/com/hitesh/finalexam/test/HiteshITE5435FinalExamTests.java
@@ -1,10 +1,10 @@
-package com.amm.spring.finalex;
+package com.hitesh.finalexam.test;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AbdulMubeenIte5435FinalExamApplicationTests {
+class HiteshITE5435FinalExamTests {
 
 	@Test
 	void contextLoads() {
